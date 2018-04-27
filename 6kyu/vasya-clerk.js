5d67c26001aca5bed98f3723f33b2a2b3aabaae7
jonghan.kim <brownie4597@gmail.com>
2018-04-27 14:28:09 +0900

    init

---
+++ 6kyu/vasya-clerk.js
@@ -0,0 +1,32 @@
+function tickets(peopleInLine) {
+  var budget = {
+    25: 0,
+    50: 0,
+    100: 0,
+  }
+  
+  for(let item of peopleInLine) {
+    if(item - 25 == 0)
+      budget[25] += 1
+    else if(item - 25 == 25) {
+      budget[25] -= 1
+      budget[50] += 1
+    }
+    else {
+      if(budget[50] > 0) {
+        budget[50] -= 1
+        budget[25] -= 1
+      }
+      else {
+        budget[25] -= 3
+      }
+    }
+
+    if(budget[25] < 0 || budget[50] < 0)
+      return "NO"
+  }
+  return "YES"
+}
+
+console.log(tickets([25, 25, 50, 50]) === "YES" ? "PASSED" : "FAILED")
+console.log(tickets([25, 100]) === "NO" ? "PASSED" : "FAILED")
