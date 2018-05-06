9c239db6e64b01b0a995c40ab96c33c6275604f4
JONG HAN KIM <brownie4597@gmail.com>
2018-05-06 22:02:09 +0900

    python

--- python/MissingNumberInArithmetic.py
+++ python/MissingNumberInArithmetic.py
@@ -3,3 +3,7 @@ def find(seq):
     return (len(seq)+1) * (2*min(seq) + (len(seq))*d) / 2 - sum(seq)
 
 find([3, 9, 1, 11, 13, 5])
+
+# Clever
+# def find(seq):
+#   return (min(seq)+max(seq))*(len(seq)+1)/2-sum(seq)
